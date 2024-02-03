b14d6afeade1f566e178497bbaaeacee34ce330a
jatinssh <jatin7838066@gmail.com>
2024-02-03 23:44:55 +0530

    updated

---
+++ taskflow/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import './Navbar.css'
+const Navbar = ()=>{
+    return (
+        <div className='navbar'>
+            <div className="navlogo">
+                <img src=""></img>
+
+            </div>
+            <ul className="nav-menu">
+            <li>Features</li>
+                    <li>Team</li>
+                    <li>Login</li>
+                    
+
+            </ul>
+            <nav>
+                <button>Check Demo</button>
+                
+            </nav>
+        </div>
+    )
+
+}
+export default Navbar;
